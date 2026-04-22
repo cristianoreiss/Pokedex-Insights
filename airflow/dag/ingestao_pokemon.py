2fae030231fe3869950025ae2113f93c9c57964c
Cristiano Reis <cristianoreis0515@gmail.com>
2026-04-22 08:54:17 -0300

    correção da lógica do while

--- airflow/dag/ingestao_pokemon.py
+++ airflow/dag/ingestao_pokemon.py
@@ -15,20 +15,17 @@ collection = db['bronze_pokemon']
 
 def ingestao_pokemon():
     url_paginacao = "https://pokeapi.co/api/v2/pokemon/"
-    response = requests.get(url_paginacao)
-    dados_brutos = response.json()
-    url_detalhes_pokemon = url_paginacao
 
-    while (url_detalhes_pokemon):
+    while (url_paginacao):
+        response = requests.get(url_paginacao)
+        dados_brutos = response.json()
         lista_pokemons = dados_brutos['results']
         lista_para_input = []
         for pokemon in lista_pokemons:
             detalhes_pokemon = requests.get(pokemon['url']).json()
             lista_para_input.append(detalhes_pokemon)
         collection.insert_many(lista_para_input)
-        url_detalhes_pokemon = dados_brutos['next']
-        response = requests.get(url_detalhes_pokemon)
-        dados_brutos = response.json()
+        url_paginacao = dados_brutos['next']
 
 
 if __name__ == "__main__":
@@ -36,4 +33,4 @@ if __name__ == "__main__":
 
 ## 1 - Implementar o upsert do mongo para realizar a substituição
 ## 2 - Implementar o tratamento de erro para o caso de não encontrar algum registro
-## 3 - Corrigir o while: "Python while True with conditional break": >> Tente inverter a ordem dentro do while. Em vez de buscar a "próxima página" no final do loop para usá-la na volta seguinte, tente estruturar de forma que o get da lista seja a primeira coisa a acontecer.
+## 3 (OK) - Corrigir o while: "Python while True with conditional break": >> Tente inverter a ordem dentro do while. Em vez de buscar a "próxima página" no final do loop para usá-la na volta seguinte, tente estruturar de forma que o get da lista seja a primeira coisa a acontecer.
