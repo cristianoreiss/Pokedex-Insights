cf97de99c93fd71e2d73234f0fdb0678f18ba47c
Cristiano Reis <cristianoreis0515@gmail.com>
2026-04-23 16:23:20 -0300

    feat: implemtação do tratamento de erros

--- airflow/dag/ingestao_pokemon.py
+++ airflow/dag/ingestao_pokemon.py
@@ -17,22 +17,30 @@ def ingestao_pokemon():
     url_paginacao = "https://pokeapi.co/api/v2/pokemon/"
 
     while (url_paginacao):
-        response = requests.get(url_paginacao)
-        dados_brutos = response.json()
-        lista_pokemons = dados_brutos['results']
-        lista_para_input = []
-        for pokemon in lista_pokemons:
-            detalhes_pokemon = requests.get(pokemon['url']).json()
-            operacao = ReplaceOne(
-                {"id": detalhes_pokemon["id"]},
-                detalhes_pokemon,
-                upsert=True
-            )
-            lista_para_input.append(operacao)
-        collection.bulk_write(lista_para_input)
-        url_paginacao = dados_brutos['next']
+        try:
+            response = requests.get(url_paginacao)
+            dados_brutos = response.json()
+            lista_pokemons = dados_brutos['results']
+            lista_para_input = []
+            for pokemon in lista_pokemons:
+                try:
+                    detalhes_pokemon = requests.get(pokemon['url']).json()
+                except Exception as f:
+                    print(f"Erro ao pegar as informações: {f}")
+                    continue
+                operacao = ReplaceOne(
+                    {"id": detalhes_pokemon["id"]},
+                    detalhes_pokemon,
+                    upsert=True
+                )
+                lista_para_input.append(operacao)
+            if lista_para_input:
+                collection.bulk_write(lista_para_input)
+            url_paginacao = dados_brutos['next']
+        except Exception as e:
+            print(f"Ocorreu o seguinte erro: {e}")
+            break
     print("Inserção feita!")
-
 
 if __name__ == "__main__":
     ingestao_pokemon()
