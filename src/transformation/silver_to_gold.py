8e5cde240c052182857938774d6b87267b8ebbc8
Cristiano Reis <cristianoreis0515@gmail.com>
2026-07-25 20:07:07 -0300

    feat: criação da tabela unificada na camada gold

--- src/transformation/silver_to_gold.py
+++ src/transformation/silver_to_gold.py
@@ -5,3 +5,19 @@ raiz_projeto = Path(__file__).resolve().parents[2]
 caminho_dados_silver = raiz_projeto / "data" / "silver"
 caminho_dados_gold = raiz_projeto / "data" / "gold"
 caminho_dados_gold.mkdir(parents=True,exist_ok=True)
+
+pokemon = pl.read_parquet(caminho_dados_silver / "pokemon_silver.parquet")
+pokemon_stats = pl.read_parquet(caminho_dados_silver / "pokemon_stats_silver.parquet")
+pokemon_type = pl.read_parquet(caminho_dados_silver / "pokemon_types_silver.parquet")
+
+##print(pokemon)
+##print(pokemon_stats)
+
+pokemon_table = pokemon.join(pokemon_stats,on="id",how="left")
+
+pokemon_table = pokemon_table.pivot(
+    index=["id", "name", "height", "weight", "base_experience"],
+    on="stat_name",
+    values="base_stat"
+)
+print(pokemon_table)
