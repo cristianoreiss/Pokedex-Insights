27bf1b4884726e4d7ac02fd370ccd4eaa50579da
Cristiano Reis <cristianoreis0515@gmail.com>
2026-08-09 00:07:53 -0300

    feat: definição do conteúdo da página

--- app/main.py
+++ app/main.py
@@ -5,12 +5,29 @@ raiz_projeto = Path(__file__).resolve().parents[1]
 
 
 st.set_page_config(
-    page_title="Pokemon",
+    page_title="Pokedex Insights",
     page_icon=raiz_projeto / "images" / "pokemon.png",
     layout="centered"
 
 )
 
-st.title("Meu Primeiro App com Streamlit")
-st.write("Olá, mundo!")
+st.title("Pokedex Insights")
 
+filtros_laterais, tela_1 = st.columns([1, 3])
+with filtros_laterais:
+    option = st.selectbox(
+        "Stat",
+        ["hp","attack","defense"],
+        placeholder="Choose a stat",
+        accept_new_options=False
+    )
+    st.write(f"You selected:", option)
+
+    option_type = st.multiselect(
+        "Type",
+        ["fire","water","earth","plant","fly"],
+        default=None
+    )
+    
+with tela_1:
+    st.write('Primeira Tela')
